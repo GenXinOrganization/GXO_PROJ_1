ad39a5c2a0632db3ed2003b9c1e18b0ccff8041e
INCUBUS <409362416@qq.com>
2018-09-20 10:19:18 +0800

    gxoproj1项目首次提交

---
+++ gxo_proj_1/gxo_proj_1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gxo_proj_1
+//
+//  Created by Peanut Lee on 2018/9/20.
+//  Copyright © 2018 GXO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
